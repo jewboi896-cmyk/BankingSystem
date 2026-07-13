3a5f624c155a888551dff99be1c17afce6af0918
derek <derekh789@icloud.com>
2026-07-12 20:49:54 -0400

    removed userID param to force use of RequestContext

--- src/main/java/com/bank/api/dto/request/CreateCheckingRequest.java
+++ src/main/java/com/bank/api/dto/request/CreateCheckingRequest.java
@@ -3,5 +3,5 @@ package com.bank.api.dto.request;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public record CreateCheckingRequest(UUID userID, BigDecimal overdraftLimit) {
+public record CreateCheckingRequest(BigDecimal overdraftLimit) {
 }
