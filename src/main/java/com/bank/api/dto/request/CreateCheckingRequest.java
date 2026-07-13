5a98041b3aa6a348c0a194a5f2675b2052d12bce
derek <derekh789@icloud.com>
2026-07-12 20:50:57 -0400

    format fix

--- src/main/java/com/bank/api/dto/request/CreateCheckingRequest.java
+++ src/main/java/com/bank/api/dto/request/CreateCheckingRequest.java
@@ -1,7 +1,5 @@
 package com.bank.api.dto.request;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
-public record CreateCheckingRequest(BigDecimal overdraftLimit) {
-}
+public record CreateCheckingRequest(BigDecimal overdraftLimit) {}
