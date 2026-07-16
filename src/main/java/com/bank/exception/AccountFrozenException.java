5412dc88edc945f5543117215f77be2673de328c
derek <derekh789@icloud.com>
2026-07-15 21:02:07 -0400

    added FrozenOp enum to make error message more descriptive

--- src/main/java/com/bank/exception/AccountFrozenException.java
+++ src/main/java/com/bank/exception/AccountFrozenException.java
@@ -1,9 +1,27 @@
 package com.bank.exception;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.UUID;
 
 public class AccountFrozenException extends BankingException {
-    public AccountFrozenException(UUID accountID) {
-        super("Account: " + accountID + " has been frozen. No transactions are allowed to or from this account.");
+
+    public enum FrozenOp {
+        TRANSACTION,
+        CLOSURE;
+
+        @Contract(pure = true)
+         private @NotNull String describe() {
+            return switch (this) {
+                case CLOSURE -> "Account must be unfrozen before it can be " +
+                        "closed";
+                case TRANSACTION -> "Account must be unfrozen before any " +
+                        "transactions are allowed to happen";
+            };
+        }
+    }
+    public AccountFrozenException(UUID accountID, @NotNull FrozenOp op) {
+        super("Account: " + accountID + " has been frozen. " + op.describe());
     }
 }
